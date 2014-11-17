0d8f67c9981f3014e36bdcfdd3fe5b1e1acc8387
Abhinav Sharma <sharma55@illinois.edu>
2014-11-17 00:14:58 -0600

    version change

--- run_ekstazi.sh
+++ run_ekstazi.sh
@@ -8,20 +8,69 @@ if [ -z "$1" ]; then
 	exit 0
 fi
 
-project_url=$1
-project="mvn_test_project"
-version="3.4.2"
+default_location="mvn_test_project"
+project_url=""
+project=${default_location}
+version="4.2.0"
 git_project="0"
+surefire_version="0.0"
+modules="."
 
-rm -rf ${project}
+while getopts ":u:p:v:s:m:h" opt; do
+  case $opt in
+  	u)
+      #echo "-url was triggered! Parameter: ${OPTARG}" >&2
+      project_url=${OPTARG};
+      ;;
+    p)
+      #echo "-pname was triggered! Parameter: ${OPTARG}" >&2
+      project=${OPTARG}
+      ;;
+    v)
+      #echo "-ek_version was triggered! Parameter: ${OPTARG}" >&2
+      version=${OPTARG}
+      ;;
+    s)
+      #echo "-surefire_version was triggered! Parameter: ${OPTARG}" >&2
+      surefire_version=${OPTARG}
+      ;;
+   	m)
+   	  #echo "-modules was triggered! Parameter: ${OPTARG}" >&2
+   	  modules=(${OPTARG//,/ })
+      ;; 
+    h)
+	  echo "help-> ./run_ekstazi.sh -u [svn/git project url] -p [local_project_folder_name] -v [ekstazi_version] -s [forced_surefire_version] -m [modules separated by ,]" >&2
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
 
-if [ ! -z "$2" ]; then
-	project=$2
-fi
+surefire_check=$(awk 'BEGIN{ print "'${surefire_version}'"=="0.0" }')
+#echo "Surefire Version"
+#echo $surefire_check
 
-if [ ! -z "$3" ]; then
-	version=$3
-fi
+echo $project_url
+echo $project
+echo $version
+echo $surefire_version
+
+for index in "${!modules[@]}"
+do
+    echo -n "${modules[index]},"
+done
+echo ''
+
+rm -rf ${default_location}
+
+# if [ ! -z "$2" ]; then
+# 	project=$2
+# fi
+
+# if [ ! -z "$3" ]; then
+# 	version=$3
+# fi
 
 if [ ! -d "${project}" ]; then
 
@@ -29,8 +78,11 @@ if [ ! -d "${project}" ]; then
 	git_project_url="git:${protocol}"
 	#echo $protocol
 	#echo $git_project_url
+	git_project=`timeout 5 git ls-remote $project_url| wc -l | tail -1 | awk '{if ($1 > 0) {print "0"} else {print "1"}}'`
 
-	git_project=`timeout 2 git ls-remote $git_project_url | wc -l | tail -1 | awk '{if ($1 > 0) {print "0"} else {print "1"}}'`
+	if [ $git_project -ne "0" ]; then 
+		git_project=`timeout 5 git ls-remote $git_project_url | wc -l | tail -1 | awk '{if ($1 > 0) {print "0"} else {print "1"}}'`
+	fi
 
 	if [ $git_project -eq "0" ]; then
 		echo "A Git project"
@@ -62,14 +114,35 @@ else
 fi
 
 # Download Ekstazi
-url="mir.cs.illinois.edu/gliga/projects/ekstazi/release/"
-if [ ! -e org.ekstazi.core-${version}.jar ]; then wget "${url}"org.ekstazi.core-${version}.jar; fi
-if [ ! -e org.ekstazi.ant-${version}.jar ]; then wget "${url}"org.ekstazi.ant-${version}.jar; fi
-if [ ! -e ekstazi-maven-plugin-${version}.jar ]; then wget "${url}"ekstazi-maven-plugin-${version}.jar; fi
+# url="mir.cs.illinois.edu/gliga/projects/ekstazi/release/"
+# if [ ! -e org.ekstazi.core-${version}.jar ]; then wget "${url}"org.ekstazi.core-${version}.jar; fi
+# if [ ! -e org.ekstazi.ant-${version}.jar ]; then wget "${url}"org.ekstazi.ant-${version}.jar; fi
+# if [ ! -e ekstazi-maven-plugin-${version}.jar ]; then wget "${url}"ekstazi-maven-plugin-${version}.jar; fi
 
 # Install Ekstazi
-mvn install:install-file -Dfile=org.ekstazi.core-${version}.jar -DgroupId=org.ekstazi -DartifactId=org.ekstazi.core -Dversion=${version} -Dpackaging=jar -DlocalRepositoryPath=$HOME/.m2/repository/
-mvn install:install-file -Dfile=ekstazi-maven-plugin-${version}.jar -DgroupId=org.ekstazi -DartifactId=ekstazi-maven-plugin -Dversion=${version} -Dpackaging=jar -DlocalRepositoryPath=$HOME/.m2/repository/
+#mvn install:install-file -Dfile=org.ekstazi.core-${version}.jar -DgroupId=org.ekstazi -DartifactId=org.ekstazi.core -Dversion=${version} -Dpackaging=jar -DlocalRepositoryPath=$HOME/.m2/repository/
+#mvn install:install-file -Dfile=ekstazi-maven-plugin-${version}.jar -DgroupId=org.ekstazi -DartifactId=ekstazi-maven-plugin -Dversion=${version} -Dpackaging=jar -DlocalRepositoryPath=$HOME/.m2/repository/
 
-java -jar ../pom_parser.jar . "${version}"	
-../test_ekstazi.sh .
+mvn install -DskipTests
+
+for index in "${!modules[@]}"
+
+do
+	echo "In project ${modules[index]},"
+	
+	cd ${modules[index]}
+	
+	if [ $surefire_check -eq "1" ]; then
+		java -jar ${cwd}/pom_parser.jar "${cwd}/${project}" "${version}" 	
+	else
+		java -jar ${cwd}/pom_parser.jar "${cwd}/${project}" "${version}" "${surefire_version}" 	
+	fi
+    
+	find ./ -iname "*pom.xml*" -exec sed -i 's/^\s*<argLine/<\!-- <argLine/g' {} \;
+	find ./ -iname "*pom.xml*" -exec sed -i 's/<\/argLine>\s*$/<\/argLine> -->/g' {} \;
+
+	${cwd}/test_ekstazi.sh .
+
+done
+
+cd ${cwd}
