c859f0120ba4a77a011fe042f301358736f5cd12
Abhinav Sharma <sharma55@illinois.edu>
2014-11-26 23:01:36 -0600

    Test script logging changed

--- test_ekstazi.sh
+++ test_ekstazi.sh
@@ -2,23 +2,37 @@
 
 cd $1
 
+cwd=`pwd`
+
+tfile1=".ekstazi_mvn_run_t1"
+tfile2=".ekstazi_mvn_run_t2"
+
+
 find ./ -iname ".ekstazi" -exec rm -rf {} \;
 
 
 t1=$(date +"%s")
 
-trun1=`mvn test -fae | tee /dev/tty | awk '/Results :/{y=1;next}y'| grep -i "Tests run: " | awk '{print $3}' | cut -d',' -f 1`
+mvn test -fae | tee ${tfile1}
+
+trun1=`awk '/Results :/{y=1;next}y' ${tfile1} | grep -i "Tests run: " | awk '{print $3}' | cut -d',' -f 1`
+
+rm -rf ${tfile1}
 
 t2=$(date +"%s")
 
-trun2=`mvn test -fae | tee /dev/tty | awk '/Results :/{y=1;next}y'| grep -i "Tests run: " | awk '{print $3}' | cut -d',' -f 1`
+mvn test -fae | tee ${tfile2}
+
+trun2=`awk '/Results :/{y=1;next}y' ${tfile2} | grep -i "Tests run: " | awk '{print $3}' | cut -d',' -f 1`
+
+rm -rf ${tfile2}
 
 t3=$(date +"%s")
 
 if [ -z "$trun1" ]
 then
 	trun1=0
-	echo "Some problem in running tests"
+	echo "Some problem in running tests" 
 	exit 0
 fi
 
@@ -46,26 +60,30 @@ for ekFolder in ${ek_folders}; do
 done
 
 
-IFS=', ' read -a tests_array <<< "$trun1"
-trun1=0
-for element in "${tests_array[@]}"
+strun1=0
+#IFS=', ' read -a tests_array <<< "$trun1"
+#for element in "${tests_array[@]}"
+for element in ${trun1//\s/ } ;
 do
-	trun1=$(( $trun1 + $element ))
+	strun1=$(( $strun1 + $element ))
 done
+trun1=$strun1
 
-IFS=', ' read -a ek_tests_array <<< "$trun2"
-trun2=0
-for element in "${ek_tests_array[@]}"
+strun2=0
+#IFS=', ' read -a ek_tests_array <<< "$trun2"
+#for element in "${ek_tests_array[@]}"
+for element in ${trun2//\s/ } ;
 do
-	trun2=$(( $trun2 + $element ))
+	strun2=$(( $strun2 + $element ))
 done
+trun2=$strun2
 
 time_diff=$(($t2-$t1))
 time_diff2=$(($t3-$t2))
 
 echo ""
 echo "Time taken for first run = $(($time_diff / 60)) minutes and $(($time_diff % 60)) seconds."
-echo "Time taken for first run = $(($time_diff2 / 60)) minutes and $(($time_diff2 % 60)) seconds."
+echo "Time taken for second run = $(($time_diff2 / 60)) minutes and $(($time_diff2 % 60)) seconds."
 
 
 echo ""
