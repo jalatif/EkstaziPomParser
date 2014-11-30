0cc6ad60498e974ec17dfb66b0b88f16515c9c51
sharma55-umehrot2 <abhinavsharma3105@gmail.com>
2014-11-30 06:08:00 -0600

    Multiple revisions run supported

--- test_ekstazi.sh
+++ test_ekstazi.sh
@@ -2,13 +2,15 @@
 
 cd $1
 
+log_file=$2
+
 cwd=`pwd`
 
 tfile1=".ekstazi_mvn_run_t1"
 tfile2=".ekstazi_mvn_run_t2"
 
 
-find ./ -iname ".ekstazi" -exec rm -rf {} \;
+find ./ -iname ".ekstazi" -exec rm -rf {} \; 2> /dev/null
 
 
 t1=$(date +"%s")
@@ -32,7 +34,7 @@ t3=$(date +"%s")
 if [ -z "$trun1" ]
 then
 	trun1=0
-	echo "Some problem in running tests" 
+	echo "Some problem in running tests"  | tee -a ${log_file}
 	exit 0
 fi
 
@@ -50,11 +52,11 @@ for ekFolder in ${ek_folders}; do
 		#echo -n $ekFolder
 		#echo -n $ekstazi_gen
 		module_name=`echo $ekFolder  | sed -e 's/\/\.ekstazi//g' | sed -e 's/^\.\///g'`
-		echo -n "Module -> ${module_name} -> "
+		echo -n "Module -> ${module_name} -> " | tee -a ${log_file}
 		if [ $ekstazi_gen -eq "1" ]; then
-			echo "Passed"
+			echo "Passed" | tee -a ${log_file}
 		else
-			echo "Failed"
+			echo "Failed" | tee -a ${log_file}
 		fi
 	fi
 done
@@ -81,30 +83,30 @@ trun2=$strun2
 time_diff=$(($t2-$t1))
 time_diff2=$(($t3-$t2))
 
-echo ""
-echo "Time taken for first run = $(($time_diff / 60)) minutes and $(($time_diff % 60)) seconds."
-echo "Time taken for second run = $(($time_diff2 / 60)) minutes and $(($time_diff2 % 60)) seconds."
+echo "" | tee -a ${log_file}
+echo "Time taken for first run = $(($time_diff / 60)) minutes and $(($time_diff % 60)) seconds." | tee -a ${log_file}
+echo "Time taken for second run = $(($time_diff2 / 60)) minutes and $(($time_diff2 % 60)) seconds." | tee -a ${log_file}
 
 
-echo ""
-echo "Number of tests that ran first time = " $trun1
-echo "Number of tests that ran second time = " $trun2
-echo ""
+echo "" | tee -a ${log_file}
+echo "Number of tests that ran first time = " $trun1 | tee -a ${log_file}
+echo "Number of tests that ran second time = " $trun2 | tee -a ${log_file}
+echo "" | tee -a ${log_file}
 
 tdiff=`expr ${trun1} - ${trun2}`
 
-echo "Difference of testcases that ran = " $tdiff
+echo "Difference of testcases that ran = " $tdiff | tee -a ${log_file}
 
 if [ $tdiff -eq $trun1 ]
 then
-	echo "Tests reduced 100% successfully"
+	echo "Tests reduced 100% successfully" | tee -a ${log_file}
 elif [ $tdiff -gt 0 ]
 then
-	echo "Some of the tests were reduced by ekstazi."
+	echo "Some of the tests were reduced by ekstazi." | tee -a ${log_file}
 elif [ $tdiff -eq 0 ]
 then	
-	echo "Ekstazi didn't ran successfully. Check pom configuration again."
+	echo "Ekstazi didn't ran successfully. Check pom configuration again." | tee -a ${log_file}
 else
-	echo "Something wrong here. Check pom configuration again."
+	echo "Something wrong here. Check pom configuration again." | tee -a ${log_file}
 fi
 
